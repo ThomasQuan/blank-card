3b0a81807edcd438c6fda25bbb23cfd0c95194c0
Thomas Quan <quantrithanh1999@gmail.com>
2023-01-12 23:48:15 -0500

    setup complete - moving to mac for dev

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	singleQuote: false,
+	semi: true,
+	bracketSpacing: true,
+	arrowParens: "always",
+	endOfLine: "lf",
+	trailingComma: "none"
+};
